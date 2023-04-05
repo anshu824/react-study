6aeae36df000fb17f8d3afff36eb2d7538c53d36
anshu824 <anshu.kumar824@gmail.com>
2023-04-06 00:12:08 +0530

    added remaining

---
+++ 01-starting-project_4/src/components/Users/AddUser.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+import ErrorModel from "../UI/ErrorModel";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      setEror({
+        title: "Invalid Input!",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      setEror({
+        title: "Invalid Input!",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
+    }
+    props.onAddUser(enteredUserName, enteredAge);
+    setEnteredUserName("");
+    setEnteredAge("");
+  };
+
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [error, setEror] = useState();
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const errorHandler = () =>{
+    setEror(null);
+  }
+
+  return (
+    <div>
+      {error && <ErrorModel title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">User Name</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUserName}
+            onChange={usernameChangeHandler}
+          ></input>
+          <label htmlFor="age">Age(Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default AddUser;
